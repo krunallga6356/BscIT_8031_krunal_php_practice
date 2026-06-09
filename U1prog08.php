9ebee82f9c28a975086be68acab546eace500b03
krunallga6356 <krunalgangajaliya202@gmail.com>
2026-06-09 11:06:22 +0530

    Add form to generate and submit array values

--- U1prog08.php
+++ U1prog08.php
@@ -6,12 +6,49 @@
     <title>Document</title>
 </head>
 <body>
-    <?php 
-        $Names = array("Krunal","Vansh","Raj","B");
-        
-        foreach($Names as $Element){
-            echo $Element."<br>";
-        }
+
+    <?php
+
+        error_reporting(0);
+
     ?>
+
+  
+        <form method="get">
+
+            Enter number of values: 
+            <input type="number" name="n" required>
+            <input type="submit" name="generate" value="Generate">
+            
+        </form>
+
+
+        <form method = "get">
+
+            <?php
+
+                $n = $_GET['n'];
+
+                for ($i = 0; $i < $n; $i++) {
+                    echo "Value " . ($i + 1) . "<input type='text' name='arr[]'><br><br>";
+                }
+
+            ?>
+
+                <input type="submit" name="submit" value="Submit">
+
+        </form>
+
+        <?php
+        
+
+            $array = $_GET['arr'];
+
+            foreach ($array as $ele) {
+                echo $ele . "<br>";
+            }
+        
+    ?>
+
 </body>
 </html>
